23c1c3d497b30de5a7aa0c3fbf0dc344c67d2212
mridul <mridulvij1569@gmail.com>
2023-05-01 16:01:12 +0530

    connect database with backend for user storage

--- router/auth.js
+++ router/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models/userSchema');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -8,8 +9,26 @@ router.get('/', (req, res) => {
 
 // for taking input data from user in server and also in database
 router.post('/register', (req, res)=>{
-    console.log(req.body);
-    res.json({message:req.body});
-})
+
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error: "Please Fill Complete Details"});
+    }
+   // else console.log(name);
+    // //res.json({});
+
+    User.findOne({email: email })
+    .then((userExist) => {
+        if(userExist){
+            return res.status(422).json({ error: "Email Already Exist!"});
+        }
+        const user = new User({name, email, work, password, cpassword});
+        //
+    })
+});
+
+// we can define routes here in which we can get api endpoints and working of Particular endpoint in it.
+
 
 module.exports = router;
